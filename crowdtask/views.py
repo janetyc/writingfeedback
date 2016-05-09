581d4abb422a8d8b19e328c94a167089252dc001
janetyc <janetyc@gmail.com>
2016-05-09 23:12:37 +0800

    crowd task front end

--- crowdtask/views.py
+++ crowdtask/views.py
@@ -11,6 +11,7 @@ def index():
 @views.route('/topic')
 def topic_task():
     data = {
+        'title': "Criosity Killed the Cat, but Makes Crowdwork Better",
         'content': "Crowdsourcing systems organize individuals to solve complex problems that are otherwise difficult to solve computationally [cited]. A key challenge in creating these systems is to sufficiently incentivize individuals to participate and keep producing high quality work. In support of this goal, a wide range of incentive mechanisms have been designed and studied in crowdsourcing and social computing settings, including monetary payment [cited], gamification techniques [cited], social comparisons [cited], visualizations and facilitation [cited], and virtual reward systems [cited]. The efficacy of these approaches ranges quite a bit, making this an active, open research area."
     }
     print data['content']
@@ -18,8 +19,21 @@ def topic_task():
 
 @views.route('/relevance')
 def relevance_task():
-    return render_template('relevance_task.html')
+    data = {
+        'title': "Criosity Killed the Cat, but Makes Crowdwork Better",
+        'content': "Crowdsourcing systems organize individuals to solve complex problems that are otherwise difficult to solve computationally [cited]. A key challenge in creating these systems is to sufficiently incentivize individuals to participate and keep producing high quality work. In support of this goal, a wide range of incentive mechanisms have been designed and studied in crowdsourcing and social computing settings, including monetary payment [cited], gamification techniques [cited], social comparisons [cited], visualizations and facilitation [cited], and virtual reward systems [cited]. The efficacy of these approaches ranges quite a bit, making this an active, open research area.",
+        'topic_sentence': [0, 3]
+    }
+    return render_template('relevance_task.html', data=data)
 
 @views.route('/relation')
 def relation_task():
-    return render_template('relation_task.html')
+    content = "Crowdsourcing systems organize individuals to solve complex problems that are otherwise difficult to solve computationally [cited]. A key challenge in creating these systems is to sufficiently incentivize individuals to participate and keep producing high quality work. In support of this goal, a wide range of incentive mechanisms have been designed and studied in crowdsourcing and social computing settings, including monetary payment [cited], gamification techniques [cited], social comparisons [cited], visualizations and facilitation [cited], and virtual reward systems [cited]. The efficacy of these approaches ranges quite a bit, making this an active, open research area."
+    content_list = content.split(".")
+
+    data = {
+        'title': "Criosity Killed the Cat, but Makes Crowdwork Better",
+        'content_list': content_list,
+        'pair_ids': [1,2],
+    }
+    return render_template('relation_task.html', data=data)
