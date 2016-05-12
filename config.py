11aa9902be0f5297a84739001099ecb02d8ccddc
janetyc <janetyc@gmail.com>
2016-05-13 00:17:55 +0800

    change database from sqlite to postgres

--- config.py
+++ config.py
@@ -8,29 +8,34 @@ class Config(object):
     TESTING = False
 
     #DATABASE CONFIGURATION
-    SQLALCHEMY_DATABASE_URI = "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    #SQLALCHEMY_DATABASE_URI = "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/feedback"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost/feedback"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/feedback"
+    #SQLALCHEMY_DATABASE_URI = "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost/feedback"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DebugConfig(Config):
     DEBUG = True
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///%s/%s.sqlite" % (BASE_DIR, PROJECT_NAME)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost/feedback"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
