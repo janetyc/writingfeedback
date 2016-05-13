f681d2e034dba662acfd26674ecdaa7945e24d87
janetyc <janetyc@gmail.com>
2016-05-14 01:07:02 +0800

    add enumerations file

---
+++ enum.py
@@ -0,0 +1,14 @@
+def enum(*sequential, **named):
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    reverse = dict((value, key) for key, value in enums.iteritems())
+    enums['reverse_mapping'] = reverse
+    return type('Enum', (), enums)
+
+TaskType = enum(
+    TOPIC="topic", RELEVANCE="relevance", RELATION="relation"
+)
+
+RelationType = enum(
+    ADDITION="Addition", SEQUENTIAL_ORDER="Sequential Order", COMPARE="Compare", CONTRAST="Contrast", CAUSE="Cause", EFFECT="Effect", GENERATION="Generalization", SPECIFICATION="Specification", OTHERS="Others")
+
+Status = enum(REVIEW=1, ACCEPT=2, REJECT=3)
