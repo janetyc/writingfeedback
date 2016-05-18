9d4c1c8e63ede1d6214f273b9a1b89a808aa7080
janetyc <janetyc@gmail.com>
2016-05-18 09:17:05 +0800

    remove ssl_context

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from crowdtask import create_app
 env = os.getenv('ENV')
 port = int(os.environ.get('PORT', 5000))
 app = create_app(env)
-#app.run(host='0.0.0.0', port=port, debug=app.debug, ssl_context='adhoc')
-app.run(host='0.0.0.0', port=port, debug=app.debug, ssl_context=('ssl.cert','ssl.key'))
+app.run(host='0.0.0.0', port=port, debug=app.debug)
+#app.run(host='0.0.0.0', port=port, debug=app.debug, ssl_context=('ssl.cert','ssl.key'))
