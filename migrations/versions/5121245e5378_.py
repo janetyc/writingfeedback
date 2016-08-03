72fad4ae02ba53f48732ba224abfc802d6f129b1
janetyc <janetyc@gmail.com>
2016-08-03 14:12:36 +0800

    db migration

---
+++ migrations/versions/5121245e5378_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 5121245e5378
+Revises: a102a1e49e62
+Create Date: 2016-08-03 14:06:06.411653
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5121245e5378'
+down_revision = 'a102a1e49e62'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
